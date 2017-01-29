12675d3a0b9c829f2fefb9985bf8877d849a9759
business <business@mbp01.ten.thomsonreuters.com>
2017-01-29 10:30:00 +0100

    --> implements javascript-bridge example & include OpenOneID javascripts

--- apme-webapp/src/main/java/ch/hackaton/apme/views/DefaultSearchContentDesign.java
+++ apme-webapp/src/main/java/ch/hackaton/apme/views/DefaultSearchContentDesign.java
@@ -1,10 +1,14 @@
 package ch.hackaton.apme.views;
 
+import com.vaadin.annotations.JavaScript;
+
 /**
  * @author apme
  *
  */
 @SuppressWarnings("serial")
+@JavaScript({ "https://blockone.thomsonreuters.com/wallet-bar/bundle.js", "https://rawgit.com/ethereum/web3.js/develop/dist/web3.min.js",
+        "vaadin://js/alert.js" })
 public class DefaultSearchContentDesign extends SearchContentDesign {
 
     public DefaultSearchContentDesign() {
